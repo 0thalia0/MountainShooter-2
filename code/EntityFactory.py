215bec9131789a601f11f1049d72c8f6fcc0e503
Thalia <thalia04525@gmail.com>
2026-03-22 21:19:40 -0300

    erros corrigidos

--- code/EntityFactory.py
+++ code/EntityFactory.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+from code.Const import WIN_WIDTH
 from code.background import Background
 
 
@@ -12,6 +13,9 @@ class EntityFactory:
                 list_bg =[]
                 for i in range(7):
                    list_bg.append(Background(f'Level1Bg{i}', (0,0)))
+                   list_bg.append(Background(f'Level1Bg{i}', (WIN_WIDTH, 0)))
                 return list_bg
+
+
 
 
